12bcf0eda6d00890e45b88a4b60a8b7f3b1b36a9
Alf Fonti <alffonti@gmail.com>
2022-06-24 13:38:51 -0300

    update const variables and addEventListener method

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordLength = 15
+const passwordLength = 15
 
-passwordEl1 = document.getElementById("password-el1")
-passwordEl2 = document.getElementById("password-el2")
+const passwordEl1 = document.getElementById("password-el1")
+const passwordEl2 = document.getElementById("password-el2")
+const generatePasswords = document.getElementById('generate-passwords');
+
+generatePasswords.addEventListener('click', function generatePasswords() {
+    const passwordOne = generateRandomPassword()
+    const passwordTwo = generateRandomPassword()
+    passwordEl1.textContent = passwordOne
+    passwordEl2.textContent = passwordTwo
+})
 
 function getRandomCharacter() {
-    let randomIndex = Math.floor( Math.random()*characters.length )
+    const randomIndex = Math.floor( Math.random()*characters.length )
     return characters[randomIndex]
 }
 
@@ -19,12 +27,7 @@ function generateRandomPassword() {
     return randomPassword
 }
 
-function generatePasswords() {
-    let passwordOne = generateRandomPassword()
-    let passwordTwo = generateRandomPassword()
-    passwordEl1.textContent = passwordOne
-    passwordEl2.textContent = passwordTwo
-}
+
 
 
 
